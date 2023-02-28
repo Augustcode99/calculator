18c30c67a9b647424fe297c80b82af797b9f874a
Augustcode99 <burakodabasi99@yahoo.com>
2023-03-01 00:47:12 +0300

    clicking any operator clears display

--- script.js
+++ script.js
@@ -4,27 +4,33 @@ buttonArray = Array.from(buttonNodeList);
 
 //write a function that will do the calculations
 let calculate = (button) => {
+  console.log("a button was clicked");
   let theScreen = document.querySelector("#printP");
   let Num;
 
-  if (!isNaN(Number(button.innerText))) {
+  if (!isNaN(button.innerText)) {
     theScreen.append(button.innerText);
   } else {
     switch (button.innerText) {
       case "AC":
         console.log("clearing display");
+        theScreen.innerText = "";
         break;
       case "÷":
         console.log("division selected");
+        theScreen.innerText = "";
         break;
       case "x":
         console.log("multiplication selected");
+        theScreen.innerText = "";
         break;
       case "-":
         console.log("subtraction selected");
+        theScreen.innerText = "";
         break;
       case "+":
         console.log("addition selected");
+        theScreen.innerText = "";
         break;
       case "=":
         console.log("calculating...");
