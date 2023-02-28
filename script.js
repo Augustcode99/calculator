5fad304fd53a9958ce7bb2040ca2bd7c536b8778
Augustcode99 <burakodabasi99@yahoo.com>
2023-03-01 00:34:09 +0300

    switch case added for operators

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
-//i need to track the numbers clicked
-//when a button that isnt a number is clicked
-//stop appending to the first number variable
-//then start a new variable and start appending to that
-//keep creating new variables until = is clicked
-//call a calculate function and assign it to bottomPrint.innerText
-//need to add event listener that checks the innerText of that button
-//and appends it to a automatically generated element that will be used
-//to get the innertext later.
-//see if theres a a math method that checks if innerText is a number
-//check if you need to first make the number a number if its a string
-//create an array of buttons with queryselectorall and add a forall loop
-// to add event listeners to all of them
-
-var firstNum;
-
+//create an array of all the buttons
 buttonNodeList = document.querySelectorAll("button");
 buttonArray = Array.from(buttonNodeList);
 
-// if (!isNaN(Number(button.innerText)))
+//write a function that will do the calculations
+let calculate = (button) => {
+  let theScreen = document.querySelector("#printP");
+  let Num;
+
+  if (!isNaN(Number(button.innerText))) {
+    theScreen.append(button.innerText);
+  } else {
+    switch (button.innerText) {
+      case "AC":
+        console.log("clearing display");
+        break;
+      case "÷":
+        console.log("division selected");
+        break;
+      case "x":
+        console.log("multiplication selected");
+        break;
+      case "-":
+        console.log("subtraction selected");
+        break;
+      case "+":
+        console.log("addition selected");
+        break;
+      case "=":
+        console.log("calculating...");
+        break;
+    }
+  }
+};
+//call calc function when a button is clicked
 buttonArray.forEach((button) => {
   button.addEventListener("click", () => {
-    let theScreen = document.querySelector("#printP");
-    if (button.innerText === "AC") {
-      theScreen.innerText = "";
-    } else if (!isNaN(Number(button.innerText))) {
-      theScreen.append(button.innerText);
-    } else if (button.innerText === "+" && firstNum) {
-    }
+    calculate(button);
   });
 });
