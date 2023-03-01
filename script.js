d7bc8e38f53e2d4ce8c0eb51121c19ff3deff55f
Augustcode99 <burakodabasi99@yahoo.com>
2023-03-01 22:59:52 +0300

    basic calc algorithm working

--- script.js
+++ script.js
@@ -11,63 +11,73 @@ let operator;
 const calculate = (button) => {
   let theScreen = document.querySelector("#printP");
 
+  //if clicked button is number show on screen
   if (!isNaN(button.innerText)) {
     while (theScreen.innerText.length < 10) {
       theScreen.append(button.innerText);
-      num2 = theScreen.innerText;
-      console.log("num1 is", num1);
-      console.log("num2 is", num2);
-      console.log("operator is", operator);
       break;
     }
-  } else {
+  }
+  //if bttn is not number, set operator and num1-num2, clear or display result
+  else {
+    switch (button.innerText) {
+      case "=":
+        num2 = theScreen.innerText;
+        console.log("calculating...");
+        console.log(Number(num1), operator, Number(num2));
+        switch (operator) {
+          case "+":
+            theScreen.innerText = Number(num1) + Number(num2);
+            break;
+          case "-":
+            theScreen.innerText = Number(num1) - Number(num2);
+            break;
+          case "*":
+            theScreen.innerText = Number(num1) * Number(num2);
+            break;
+          case "/":
+            theScreen.innerText = Number(num1) / Number(num2);
+            break;
+        }
+        //set operator down here to not override it before its used in switch
+        operator = "=";
+        break;
+    }
+    //this codes here so we dont reset num1 while setting num2
+    if (!num2) {
+      num1 = theScreen.innerText;
+    }
     switch (button.innerText) {
       case "AC":
         console.log("clearing display");
-        theScreen.innerText = "";
-        num1 = "";
-        num2 = "";
-        operator = "";
+        num1 = num2 = operator = "";
         break;
       case "÷":
         console.log("division selected");
-        num1 = theScreen.innerText;
-        num2 = "";
         operator = "/";
-        theScreen.innerText = "";
         break;
       case "x":
         console.log("multiplication selected");
-        num1 = theScreen.innerText;
-        num2 = "";
         operator = "*";
-        theScreen.innerText = "";
         break;
       case "-":
         console.log("subtraction selected");
-        num1 = theScreen.innerText;
-        num2 = "";
         operator = "-";
-        theScreen.innerText = "";
         break;
       case "+":
         console.log("addition selected");
-        num1 = theScreen.innerText;
-        num2 = "";
         operator = "+";
-        theScreen.innerText = "";
-        break;
-      case "=":
-        console.log("calculating...");
-        console.log(typeof num1);
-        console.log(typeof num2);
-        console.log(typeof operator);
         break;
     }
-    console.log("num1 is", num1);
-    console.log("num2 is", num2);
-    console.log("operator is", operator);
+    //when we select an operator always clear screen
+    if (operator !== "=") {
+      theScreen.innerText = "";
+    }
   }
+  //use below code for debugging
+  // console.log("num1 is", num1);
+  // console.log("num2 is", num2);
+  // console.log("operator is", operator);
 };
 //call calc function when a button is clicked
 buttonArray.forEach((button) => {
